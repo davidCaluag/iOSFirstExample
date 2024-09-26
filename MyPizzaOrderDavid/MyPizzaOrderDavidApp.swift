113a41cb16468266afc22a36879f3b3fc3ce23e6
davidCaluag <caluagd@sheridancollege.ca>
2024-09-26 13:11:34 -0400

    Initial Commit

---
+++ MyPizzaOrderDavid/MyPizzaOrderDavidApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPizzaOrderDavidApp.swift
+//  MyPizzaOrderDavid
+//
+//  Created by David C on 2024-09-26.
+//
+
+import SwiftUI
+
+@main
+struct MyPizzaOrderDavidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
