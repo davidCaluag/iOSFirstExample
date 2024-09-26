1b503e0df31d083eb29cc69e1d0e578cc6c71db1
davidCaluag <caluagd@sheridancollege.ca>
2024-09-26 13:13:21 -0400

    Addded the paragraph.

---
+++ MyPizzaOrderDavid/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  MyPizzaOrderDavid
+//
+//  Created by David C on 2024-09-26.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
